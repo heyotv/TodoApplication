eec4ec8bcf56541c03b12c4a89c4f12866dcbd86
Vitali Tumilovich <hey.tumilovich@gmail.com>
2021-10-03 22:50:50 +0300

    feat: added component with some styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+export interface Todo {
+  id: number
+  title: string
+  completed: boolean
+  date?: any
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'TodoApplication';
+  appTitle = 'Hello world'
+
+  public todos: Todo[] = [
+    {id: 1, title: 'Купить хлеб', completed: false, date: new Date()},
+    {id: 2, title: 'Купить масло', completed: true, date: new Date()},
+    {id: 3, title: 'Купить пиво', completed: false, date: new Date()}
+  ]
 }
