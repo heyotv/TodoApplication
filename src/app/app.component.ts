2f4ba43d1483ead3dfbb5abe07b7e91a8fb079e6
Vitali Tumilovich <hey.tumilovich@gmail.com>
2021-10-11 23:55:04 +0300

    feat: added service with some functional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,5 @@
 import { Component } from '@angular/core';
 
-export interface Todo {
-  id: number
-  title: string
-  completed: boolean
-  date?: any
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -14,10 +7,4 @@ export interface Todo {
 })
 export class AppComponent {
   appTitle = 'Hello world'
-
-  public todos: Todo[] = [
-    {id: 1, title: 'Купить хлеб', completed: false, date: new Date()},
-    {id: 2, title: 'Купить масло', completed: true, date: new Date()},
-    {id: 3, title: 'Купить пиво', completed: false, date: new Date()}
-  ]
 }
